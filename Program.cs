314a515a3cb05b9e0623730a5cdd8ec636091c49
Rustam Mirzoev <74654492+RustamMirzeyev@users.noreply.github.com>
2020-11-27 16:07:17 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace salam
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    int[] arr = { 2, -4, 8790890, 990, 9 };
+        //    int n = 9;
+
+
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        if (arr[i] == n)
+        //        {
+        //            Console.WriteLine(i);
+        //        }
+        //    }
+            //int i = 0;
+            //while (i < arr.Length)
+            //{
+
+            //    Console.WriteLine(arr[i]);
+            //    i++;
+            //}
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+            //foreach (int item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+        //    PrintNumber(5);
+        //    string a = returnRustem();
+        //    Console.WriteLine(a);
+        //    //Console.WriteLine(sum(50, 6) );
+        //    //sum(5, 6);
+        //    Console.WriteLine(isBigger(3, 2)); 
+        //}
+
+        //static void PrintNumber(int n)
+        //{
+        //    Console.WriteLine(n);
+        //}
+
+        //static string returnRustem()
+        //{
+        //    return "rustem";
+        //}
+
+        //static int sum(int a, int b)
+        //{
+        //    return a+b;
+        //}
+
+        //static void sum(int a, int b)
+        //{
+
+        //    Console.WriteLine(a+b);
+        //}
+
+        //static bool isBigger(int a, int b)
+        //{
+
+        //    if (a > b)
+        //    {
+        //        return true;
+        //    }
+        //    else
+
+        //        return false;
+        //}
+
+    }
+}
